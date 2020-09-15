aa905c014d5c044409dac2d526ddeeb3ee5008ac
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-09-15 13:14:08 +0800

    temp save

---
+++ word_embeddings/syntatic_dep.py
@@ -0,0 +1,54 @@
+import json
+import io
+import spacy
+import re
+from spacy_conll import init_parser
+
+nlp = spacy.load('en_core_web_sm')
+
+def json_generator(json_file):
+    with io.open(json_file, 'r', encoding='utf-8') as load_file:
+        for line in load_file:
+            yield json.loads(line)
+
+
+def extract_sent(json_file, store_file):
+    with io.open(store_file, 'a', encoding='utf-8') as output:
+        cnt = 0
+        for jsonObj in json_generator(json_file):
+            para = jsonObj['abstract'].strip().replace('\n', ' ')
+            latex_str = re.search(r'\$.*?\$', para)
+            while latex_str:
+                para = para.replace(latex_str.group(), '')
+                latex_str = re.search(r'\$.*?\$', para)
+            doc = nlp(para)
+            for sentence in doc.sents:
+                output.write(str(sentence) + '\n')
+            cnt += 1
+            if cnt % 1000 == 0:
+                print(cnt)
+
+
+def conll_gen(text, store_file):
+    nlp = init_parser("spacy", "en")
+    # Parse a given string
+    doc = nlp(text)
+    with io.open(store_file, 'w', encoding='utf-8') as output:
+        output.write(doc._.conll_str)
+        
+
+def analysis_data(input_txt, output_txt):
+    with io.open(input_txt, 'r', encoding='utf-8') as input_file:
+        context_dict = {}
+        for line in input_file:
+            context = line.split(' ')
+            if context[1] in context_dict.keys():
+                context_dict[context[1]].append(context[0])
+            else:
+                context_dict[context[1]] = [context[0]]
+    with io.open(output_txt, 'w', encoding='utf-8') as output_file:
+        for key, item in context_dict.items():
+            output_file.write(str(key)+'\n')
+            for word in item:
+                output_file.write('    ' + str(word) + '\n')
+            output_file.write('\n')
---
+++ word_embeddings/test.py
@@ -0,0 +1,37 @@
+import json
+import io
+import spacy
+import re
+
+bad_deps = ['aux', 'auxpass', 'cc', 'neg', 'num', 'ROOT', 'pobj', 'punct', 'det']
+examples = []
+
+nlp = spacy.load('en_core_web_sm')
+
+t = 'Neural word embeddings are often considered opaque and uninterpretable, unlike sparse vector space representations in which each dimension corresponds to a particular known context, or LDA models where dimensions correspond to latent topics. While this is true to a large extent, we observe that S KIP G RAM does allow a non-trivial amount of introspection. Although we cannot assign a meaning to any particular dimension, we can indeed get a glimpse at the kind of information being captured by the model, by examining which contexts are “activated” by a target word.'
+
+doc = nlp(t)
+
+for sentence in doc.sents:
+    for word in sentence:
+        #casing should probably be a paramater
+        source =  word.head.text.lower()
+        target= word.text.lower()
+        dep = word.dep_
+        for child in word.children:
+            source =  child.head.text.lower()
+            target= child.text.lower()
+            dep = child.dep_
+            #If we see a prepositional dependency, we want to merge it
+            #so ('scientist', prep, 'with') and ('with', pobj,'telescope) 
+            #becomes ('scientist, 'prep_with', 'telescope)
+            if dep == 'prep':
+                for c2 in child.children:
+                    if (c2.dep_ == 'pobj'):
+                        examples.append((source,"prep_" + child.text.lower(),  c2.text))
+            else:
+                if not dep in bad_deps:
+                    examples.append((source, dep,target))
+
+for item in examples:
+    print(item)
