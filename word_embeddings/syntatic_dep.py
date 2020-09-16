e86970c1d1678da4241048057e559c980164c580
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-09-16 14:27:58 +0800

    sentence processor done

--- word_embeddings/syntatic_dep.py
+++ word_embeddings/syntatic_dep.py
@@ -1,6 +1,7 @@
 import json
 import io
 import spacy
+from nltk.tokenize import word_tokenize
 import re
 from spacy_conll import init_parser
 
@@ -31,6 +32,99 @@ def extract_sent(json_file, store_file):
             if cnt % 1000 == 0:
                 print(cnt)
         print(cnt)
+
+class Dep_Based_Word_Embed:
+
+    def build_word_tree(self, input_txt):
+        with io.open(input_txt, 'r', encoding='utf-8') as load_file:
+            raw_list = load_file.readline().strip('[]').split(',')
+        word_list = (word.strip(" '") for word in raw_list)
+        
+        self.MyTree = {}
+        cnt = 0
+        for word in word_list:
+            cnt += 1
+            if ' ' not in word:
+                # If the word is an atomic word instead of a phrase
+                if word not in self.MyTree.keys():
+                    # If this is the first time that this word is inserted to the tree
+                    self.MyTree[word] = {"":""}
+                elif "" not in self.MyTree[word].keys():
+                    # If the word has been inserted but is viewed as an atomic word the first time
+                    self.MyTree[word][""] = ""
+                # If the word has already been inserted as an atomic word, then we do nothing
+            else:
+                # If the word is an phrase
+                phrase = word.split(' ')
+                length = len(phrase)
+                fw = phrase[0]
+                if fw not in self.MyTree.keys():
+                    self.MyTree[fw] = {}
+                temp_dict = self.MyTree.copy()
+                parent_node = fw
+                for i in range(1, length):
+                    sw = phrase[i]
+                    if sw not in temp_dict[parent_node].keys():
+                        # The second word is inserted to as the child of parent node the first time
+                        temp_dict[parent_node][sw] = {}
+                    if i == length - 1:
+                        # If the second word is the last word in the phrase
+                        if "" not in temp_dict[parent_node][sw].keys():
+                            temp_dict[parent_node][sw][""] = ""
+                    else:
+                        # If the second word is not the last word in the phrase
+                        temp_dict = temp_dict[parent_node].copy()
+                        parent_node = sw
+            if cnt % 1000 == 0:
+                print(cnt)
+        print('Building word tree is accomplished with {:d} words added'.format(cnt))
+        # print(self.MyTree)
+
+    def process_sent(self, sent):
+        if not sent:
+            return ''
+        word_tokens = word_tokenize(sent)
+        reformed_sent = []
+        i = 0
+        while i < len(word_tokens):
+            if word_tokens[i] not in self.MyTree.keys():
+                # If the word is not part of a key word directly add the word to the reformed_sent list
+                reformed_sent.append(word_tokens[i])
+                i += 1
+
+            else:
+                # If the word is part of a key word
+                phrase_buf = []
+                phrase_wait_buf = []
+                it = self.MyTree
+                while i < len(word_tokens) and word_tokens[i] in it.keys():
+                    # Add the word to the wait list
+                    phrase_wait_buf.append(word_tokens[i])
+                    if "" in it[word_tokens[i]].keys():
+                        # If the word could be the last word of a keyword, update the phrase buffer to be the same with wait buffer
+                        phrase_buf = phrase_wait_buf.copy()
+                    # Go down the tree to the next child
+                    it = it[word_tokens[i]]
+                    i += 1
+                # Change the keyword into one uniformed word and add it to the reformed_sent
+                reformed_sent.append('_'.join(phrase_buf))
+        return ' '.join(reformed_sent)
+
+    def process_sents(self, sent_file, reformed_sent_file):
+        with io.open(reformed_sent_file, 'w', encoding='utf-8') as output_file:
+            with io.open(sent_file, 'r', encoding='utf-8') as load_file:
+                cnt = 0
+                for line in load_file:
+                    cnt += 1
+                    if line:
+                        sent = line.strip()
+                        reformed = self.process_sent(sent)
+                        if reformed:
+                            output_file.write(reformed)
+                            output_file.write('\n')
+                    if cnt % 1000 == 0:
+                        print(cnt)
+                print('Process sentences accomplished with {:d} lines processed'.format(cnt))
 
 
 def conll_gen(text, store_file):
