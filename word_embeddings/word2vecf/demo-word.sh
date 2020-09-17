4838ac95840787c24813feac973bbd6f32a791c2
Kerui Zhu <keruiz2@owl2.cs.illinois.edu>
2020-09-17 02:53:52 -0500

    not able to run on server

--- word_embeddings/word2vecf/demo-word.sh
+++ word_embeddings/word2vecf/demo-word.sh
@@ -1,8 +1,4 @@
 make
-if [ ! -e text8 ]; then
-  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-  gzip -d text8.gz -f
-fi
 time 
 ./count_and_filter -train context.txt -cvocab cv -wvocab wv -min-count 100
 ./word2vecf -train context.txt -wvocab wv -cvocab cv -output word_embedding -size 200 -negative 15 -threads 10
