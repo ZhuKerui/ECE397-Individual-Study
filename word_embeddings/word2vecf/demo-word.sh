38ff3ba468c1894ab74db7904c0d83e1266443a7
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-09-17 16:08:09 +0800

    runnable again

--- word_embeddings/word2vecf/demo-word.sh
+++ word_embeddings/word2vecf/demo-word.sh
@@ -1,5 +1,5 @@
 make
 time 
-./count_and_filter -train context.txt -cvocab cv -wvocab wv -min-count 100
+./count_and_filter -train context.txt -cvocab cv -wvocab wv -min-count 10
 ./word2vecf -train context.txt -wvocab wv -cvocab cv -output word_embedding -size 200 -negative 15 -threads 10
 ./distance word_embedding
