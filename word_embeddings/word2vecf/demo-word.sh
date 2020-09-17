9c976a4aa2c3b48b4102aedd8753007d3d878c17
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-09-17 17:18:24 +0800

    maybe work

--- word_embeddings/word2vecf/demo-word.sh
+++ word_embeddings/word2vecf/demo-word.sh
@@ -1,5 +1,5 @@
 make
 time 
 ./count_and_filter -train ../../../dataset/dep_context.txt -cvocab cv -wvocab wv -min-count 20
-./word2vecf -train ../../../dataset/dep_context.txt -wvocab wv -cvocab cv -output word_embedding -size 200 -negative 15 -threads 10
-./distance word_embedding
+./word2vecf -train ../../../dataset/dep_context.txt -wvocab wv -cvocab cv -output word_embedding.bin -size 200 -negative 15 -threads 10 -binary 1
+./distance word_embedding.bin
