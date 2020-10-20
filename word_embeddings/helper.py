ae76e540d9ee7657702f6134f4cbd422a9c7896b
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-10-21 01:49:20 +0800

    add helper

---
+++ word_embeddings/helper.py
@@ -0,0 +1,38 @@
+import csv
+import io
+import json
+import spacy
+from nltk.tokenize import word_tokenize
+import re
+from spacy_conll import init_parser
+import numpy as np
+import sys
+
+nlp = spacy.load('en_core_web_sm')
+
+# with io.open('../../dataset/word2vec/mag_cs_keywords.csv', 'r', encoding='utf-8') as load_file:
+#     csv_reader = csv.reader(load_file)
+#     with io.open('../../dataset/word2vec/keyword.txt', 'w', encoding='utf-8') as dump_file:
+#         for row in csv_reader:
+#             dump_file.write(row[1])
+#             dump_file.write('\n')
+
+
+
+with io.open('../../dataset/filtered_arxiv.json', 'r', encoding='utf-8') as load_file:
+    with io.open('../../dataset/sent.txt', 'w', encoding='utf-8') as dump_file:
+        cnt = 0
+        for line in load_file:
+            temp = line.strip().split(':')
+            if temp[0].strip() == '"abstract"':
+                cnt += 1
+                abstract_str = ':'.join(temp[1:]).strip().replace('\n', ' ')
+                latex_str = re.search(r'\$.*?\$', abstract_str)
+                while latex_str:
+                    abstract_str = abstract_str.replace(latex_str.group(), '')
+                    latex_str = re.search(r'\$.*?\$', abstract_str)
+                doc = nlp(abstract_str)
+                for sentence in doc.sents:
+                    dump_file.write(str(sentence) + '\n')
+                if cnt % 1000 == 0:
+                    print(cnt)
