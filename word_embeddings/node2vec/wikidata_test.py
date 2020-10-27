5d08c581ead0b931c0726a995deda89eb0cce2cd
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-10-28 04:53:24 +0800

    revise wikidata_test

--- word_embeddings/node2vec/wikidata_test.py
+++ word_embeddings/node2vec/wikidata_test.py
@@ -5,23 +5,27 @@ import requests
 from wikidata.client import Client
 from wikidata.entity import Entity
 
-def find_id(keyword_file, output_file):
+def find_id(keyword_file, output_file, error_log):
     with io.open(keyword_file, 'r', encoding='utf-8') as load_file:
         with io.open(output_file, 'w', encoding='utf-8') as dump_file:
-            f_csv = csv.writer(dump_file)
-            url = 'https://www.wikidata.org/w/api.php'
-            params = {'action':'wbsearchentities','format':'json','language':'en'}
-            cnt = 0
-            for word in load_file:
-                word = word.strip()
-                params['search'] = word
-                r = requests.get(url, params=params)
-                id = r.json()['search'][0]['id']
-                label = r.json()['search'][0]['label']
-                f_csv.writerow([word, id, label])
-                cnt += 1
-                if cnt % 100 == 0:
-                    print(cnt)
+            with io.open(error_log, 'w', encoding='utf-8') as log_file:
+                f_csv = csv.writer(dump_file)
+                url = 'https://www.wikidata.org/w/api.php'
+                params = {'action':'wbsearchentities','format':'json','language':'en'}
+                cnt = 0
+                for word in load_file:
+                    word = word.strip()
+                    params['search'] = word
+                    r = requests.get(url, params=params)
+                    if r.json()['search']:
+                        id = r.json()['search'][0]['id']
+                        label = r.json()['search'][0]['label']
+                        f_csv.writerow([word, id, label])
+                    else:
+                        log_file.write(word + '\n')
+                    cnt += 1
+                    if cnt % 100 == 0:
+                        print(cnt)
 
 # instance of(P31), subclass of(P297), part of(P361), facet of(P1269), manifestation of(P1557)
 def find_related(keyword_csv, related_set):
