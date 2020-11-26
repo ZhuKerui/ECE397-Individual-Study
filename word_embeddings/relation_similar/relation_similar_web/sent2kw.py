bf3e5ea50a605e93c955c66b969112bb0a74dc9f
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-11-26 12:22:15 +0800

    revise sent2kw

--- word_embeddings/relation_similar/relation_similar_web/sent2kw.py
+++ word_embeddings/relation_similar/relation_similar_web/sent2kw.py
@@ -36,10 +36,12 @@ class Sent2KW(Dep_Based_Embed_Generator):
         self.reformed_file = reformed_file
         self.relation_record_file = relation_record_file
 
-    def extract_relation(self):
+    def extract_relation(self, start_line:int=0, lines:int=0):
         with io.open(self.reformed_file, 'r', encoding='utf-8') as load_file:
             self.relation_record = {}
             for idx, sent in enumerate(load_file):
+                if idx < start_line:
+                    continue
                 semantic_related_list = self.find_semantic_related_kw(sent)
                 if not semantic_related_list:
                     continue
@@ -49,14 +51,26 @@ class Sent2KW(Dep_Based_Embed_Generator):
                         self.relation_record[relation] = [(idx, triple[0], triple[1])]
                     else:
                         self.relation_record[relation].append((idx, triple[0], triple[1]))
-                if idx % 10000 == 0:
-                    print(idx)
+                cnt = idx - start_line
+                if cnt >= lines - 1:
+                    break
+                if cnt % 100 == 0:
+                    print('processed %.2f' %(float(cnt) * 100 / lines))
+
             with io.open(self.relation_record_file, 'w', encoding='utf-8') as dump_file:
                 json.dump(self.relation_record, dump_file)
 
     def load_relation(self):
         self.relation_record = json.load(io.open(self.relation_record_file, 'r', encoding='utf-8'))
 
+    def merge_relation(self, new_file):
+        new_json = json.load(io.open(new_file, 'r', encoding='utf-8'))
+        for key, value in new_json.items():
+            if key in self.relation_record.keys():
+                self.relation_record[key] += value
+            else:
+                self.relation_record[key] = value
+                
     def get_sent_by_relation(self, relation, count):
         if relation not in self.relation_record.keys():
             return None
