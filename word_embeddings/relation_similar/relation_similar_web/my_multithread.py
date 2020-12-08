57e527644e1e63229021c9fee2280526d683a8f1
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-12-08 15:21:10 +0800

    change multithread to multiprocess

--- word_embeddings/relation_similar/relation_similar_web/my_multithread.py
+++ word_embeddings/relation_similar/relation_similar_web/my_multithread.py
@@ -1,11 +1,12 @@
-import threading
+# import threading
+import multiprocessing
 from math import ceil
 import signal
 import io
 import os
 import time
 
-is_exit = False
+# is_exit = False
 
 def tailor(output_file, input_prefix, input_posfix, num, remove=False):
     with io.open(output_file, 'w', encoding='utf-8') as dump_file:
@@ -17,10 +18,10 @@ def tailor(output_file, input_prefix, input_posfix, num, remove=False):
             if remove:
                 os.remove(input_prefix + str(i) + input_posfix)
 
-def multithread_kill(signum, frame):
-    global is_exit
-    is_exit = True
-    print("receive a signal %d, is_exit = %d"%(signum, is_exit))
+# def multithread_kill(signum, frame):
+#     global is_exit
+#     is_exit = True
+#     print("receive a signal %d, is_exit = %d"%(signum, is_exit))
 
 def line_process_wrapper(line_operation, freq:int, input_file:str, output_file:str, id_:int=None, start_line:int=0, lines:int=0):
     # Sanity Check
@@ -28,14 +29,14 @@ def line_process_wrapper(line_operation, freq:int, input_file:str, output_file:s
         return
     id_ = '' if id_ is None else str(id_)
     # Get global variable
-    global is_exit
+    # global is_exit
     with io.open(input_file, 'r', encoding='utf-8') as f_input:
         with io.open(output_file + id_, 'w', encoding='utf-8') as f_output:
             idx = -1
             output_buffer = []
             for idx, line in enumerate(f_input):
-                if is_exit:
-                    break
+                # if is_exit:
+                #     break
                 if idx < start_line:
                     continue
                 result = line_operation(line.strip())
@@ -52,13 +53,13 @@ def line_process_wrapper(line_operation, freq:int, input_file:str, output_file:s
             # Make sure all data is written back
             if output_buffer:
                 f_output.write(''.join(output_buffer))
-            if is_exit:
-                print('Thread%s is terminated' % (id_))
+            # if is_exit:
+            #     print('Thread%s is terminated' % (id_))
             else:
                 print('Extract context accomplished with %d lines processed' % (1 + idx - start_line))
 
 def multithread_wrapper(line_operation, freq:int, input_file:str, output_file:str, thread_num:int=1):
-    global is_exit
+    # global is_exit
     
     if thread_num <= 0:
         return
@@ -72,23 +73,25 @@ def multithread_wrapper(line_operation, freq:int, input_file:str, output_file:st
 
     unit_lines = ceil(line_count / thread_num)
     threads = []
-    signal.signal(signal.SIGINT, multithread_kill)
-    signal.signal(signal.SIGTERM, multithread_kill)
-    is_exit = False
+    # signal.signal(signal.SIGINT, multithread_kill)
+    # signal.signal(signal.SIGTERM, multithread_kill)
+    # is_exit = False
     for i in range(thread_num):
         id_ = None if thread_num == 1 else i
         start_line = unit_lines * i
-        t = threading.Thread(target=line_process_wrapper, args=(line_operation, freq, input_file, output_file, id_, start_line, unit_lines))
+        t = multiprocessing.Process(target=line_process_wrapper, args=(line_operation, freq, input_file, output_file, id_, start_line, unit_lines))
         # t.setDaemon(True)
         threads.append(t)
     for i in range(thread_num):
         threads[i].start()
-    while 1:
-        alive = False
-        for i in range(thread_num):
-            alive = alive or threads[i].isAlive()
-        if not alive:
-            break
+    for i in range(thread_num):
+        threads[i].join()
+    # while 1:
+    #     alive = False
+    #     for i in range(thread_num):
+    #         alive = alive or threads[i].isAlive()
+    #     if not alive:
+    #         break
 
     if thread_num > 1:
         tailor(output_file, output_file, '', thread_num, remove=True)
