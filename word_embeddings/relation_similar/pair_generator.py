b3606e6e188ac32996486b239f6f77a5438720f9
ZhuKerui <kerui.17@intl.zju.edu.cn>
2021-01-04 18:36:33 +0800

    revise pair_generator.py

--- word_embeddings/relation_similar/pair_generator.py
+++ word_embeddings/relation_similar/pair_generator.py
@@ -5,8 +5,9 @@ from pair2vec.vocab import Vocab
 from my_keywords import *
 
 class Pair_Generator(Keywords):
-    def __init__(self, win=8):
+    def __init__(self, win=8, margin=1):
         self.win = win
+        self.margin = margin
         self.ignored_pos = set(['PUNCT', 'DET'])
         self.unk, self.pad, self.x_placeholder, self.y_placeholder = '<unk>', '<pad>', '<X>', '<Y>'
 
@@ -74,29 +75,43 @@ class Pair_Generator(Keywords):
         filtered_ids = ids[mask]
         filtered_kw_mask = kw_mask[mask]
         filtered_sent = [doc[i].text for i in filtered_ids]
-        if len(filtered_sent) < self.win:
-            append_num = self.win - len(filtered_sent)
-            filtered_sent += [self.pad] * append_num
-            filtered_kw_mask = np.append(filtered_kw_mask, [False] * append_num)
+        # if len(filtered_sent) < self.win:
+        #     append_num = self.win - len(filtered_sent)
+        #     filtered_sent += [self.pad] * append_num
+        #     filtered_kw_mask = np.append(filtered_kw_mask, [False] * append_num)
         valid_ids = [self.ctx_vocab.stoi[word] for word in filtered_sent]
         context = []
-        win_idx = np.arange(self.win)
-        for start_i in range(0, len(filtered_sent) - self.win + 1):
-            sub_kw_mask = filtered_kw_mask[start_i:start_i+self.win]
-            if sub_kw_mask.sum() <= 1:
-                continue
-            sub_sent = filtered_sent[start_i:start_i+self.win]
-            sub_valid_ids = valid_ids[start_i:start_i+self.win]
-            keyword_pairs = []
-            temp_idx = win_idx[sub_kw_mask]
-            for head in range(len(temp_idx)-1):
-                for tail in range(head+1, len(temp_idx)):
-                    keyword_pairs.append([temp_idx[head], temp_idx[tail]])
-
-            for pair in keyword_pairs:
-                int_context = [self.key_vocab.stoi[sub_sent[pair[0]]], self.key_vocab.stoi[sub_sent[pair[1]]]] + sub_valid_ids[:pair[0]] + [self.ctx_vocab.stoi[self.x_placeholder]] + sub_valid_ids[pair[0]+1:pair[1]] + [self.ctx_vocab.stoi[self.y_placeholder]] + sub_valid_ids[pair[1]+1:]
+        kw_ids = np.arange(len(filtered_kw_mask))[filtered_kw_mask]
+        for ix in range(len(kw_ids)-1):
+            for iy in range(ix + 1, len(kw_ids)):
+                x_id, y_id = kw_ids[ix], kw_ids[iy]
+                interval = y_id - x_id
+                if interval >= self.win:
+                    break
+                available_margin = self.win - (interval + 1)
+                start = max(0, x_id - min(available_margin, self.margin))
+                end = min(y_id + self.margin, start + self.win - 1)
+                int_context = [self.key_vocab.stoi[filtered_sent[x_id]], self.key_vocab.stoi[filtered_sent[y_id]]] + valid_ids[start:x_id] + [self.ctx_vocab.stoi[self.x_placeholder]] + valid_ids[x_id+1:y_id] + [self.ctx_vocab.stoi[self.y_placeholder]] + valid_ids[y_id+1:end+1]
                 context.append(' '.join(map(str, int_context)))
                 context.append('\n')
+
+        # win_idx = np.arange(self.win)
+        # for start_i in range(0, len(filtered_sent) - self.win + 1):
+        #     sub_kw_mask = filtered_kw_mask[start_i:start_i+self.win]
+        #     if sub_kw_mask.sum() <= 1:
+        #         continue
+        #     sub_sent = filtered_sent[start_i:start_i+self.win]
+        #     sub_valid_ids = valid_ids[start_i:start_i+self.win]
+        #     keyword_pairs = []
+        #     temp_idx = win_idx[sub_kw_mask]
+        #     for head in range(len(temp_idx)-1):
+        #         for tail in range(head+1, len(temp_idx)):
+        #             keyword_pairs.append([temp_idx[head], temp_idx[tail]])
+
+        #     for pair in keyword_pairs:
+        #         int_context = [self.key_vocab.stoi[sub_sent[pair[0]]], self.key_vocab.stoi[sub_sent[pair[1]]]] + sub_valid_ids[:pair[0]] + [self.ctx_vocab.stoi[self.x_placeholder]] + sub_valid_ids[pair[0]+1:pair[1]] + [self.ctx_vocab.stoi[self.y_placeholder]] + sub_valid_ids[pair[1]+1:]
+        #         context.append(' '.join(map(str, int_context)))
+        #         context.append('\n')
         return ''.join(context)
 
     def extract_context(self, freq: int, input_file: str, output_file: str, thread_num: int):
